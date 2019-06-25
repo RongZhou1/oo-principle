425e9a8bdadc4b792f869dd563d54df51d20e204
RongZhou1 <rzhou@thoughtworks.com>
2019-06-25 18:03:26 +0800

    [Length][Rong & Xin] Implement compare longer

---
+++ src/main/java/cc/oobootcamp/length/Length.java
@@ -0,0 +1,18 @@
+package cc.oobootcamp.length;
+
+public class Length {
+    private int value;
+
+    public Length(int value) {
+        this.value = value;
+    }
+
+    public boolean isLongerThan(Length length) {
+        boolean result = false;
+        if (this.value > length.value) {
+            result = true;
+        }
+        return result;
+    }
+
+}
